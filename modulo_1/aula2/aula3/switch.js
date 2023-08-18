496a077513132f0b6d9f388bb7562e6841a89f8b
ruby --version <paulinn.rs@gmail.com>
2023-08-18 17:54:23 -0300

    Switch

---
+++ modulo_1/aula2/aula3/switch.js
@@ -0,0 +1,20 @@
+const resultado = prompt("Escolha uma alternativa")
+
+const resultadoNumerico = parseFloat(resultado)
+
+switch (resultadoNumerico) {
+
+    case 1:
+    alert("O resultado e o 'Número 1'")
+    break
+    case 2:
+        alert("O resultado e o 'Número 2'")
+        break
+    case 3:
+        alert("O resultado e o 'Número 3'")
+        break
+    default:
+        alert:("Finalizando...")           
+
+}
+
