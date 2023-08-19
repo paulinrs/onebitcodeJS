4a71970e66e4d7553d643b3894fd45bac20f7c32
ruby --version <paulinn.rs@gmail.com>
2023-08-19 09:12:49 -0300

    DO WHILE

---
+++ modulo_1/aula5/do_while.js
@@ -0,0 +1,8 @@
+let temperatura = 0
+
+do{
+    alert("A temperatura da cidade é " + temperatura + " °C")
+    temperatura -= 5
+}while( temperatura > 0)
+
+alert("A temperatura final da cidade é " + temperatura + " °C") 
