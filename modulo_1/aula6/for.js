360a6573bffd426c83fe1a5c808d22cc8b5d39a2
ruby --version <paulinn.rs@gmail.com>
2023-08-20 20:48:20 -0300

    FOR

---
+++ modulo_1/aula6/for.js
@@ -0,0 +1,7 @@
+// inicialização ; condição ; finalização
+
+let nome = "Paulo" // tamanho 5 -> 0, 1, 2, 3, 4
+
+for (let indice = 0; indice <= nome.length; indice++){
+    console.log(nome[indice])
+}
