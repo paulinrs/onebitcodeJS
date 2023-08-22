ce060de66c683f5fcfbc7d731c68c2ec81405df9
ruby --version <paulinn.rs@gmail.com>
2023-08-21 21:08:48 -0300

    array

---
+++ modulo_2/aula1/array.js
@@ -0,0 +1,12 @@
+const listaDeCompras = ["Açucar", true]
+//console.log(listaDeCompras)
+
+listaDeCompras[0] = "Arroz"
+listaDeCompras[1] = "Feijão"
+listaDeCompras[2] = "Sal"
+listaDeCompras[3] = "Macarrão"
+listaDeCompras[4] = "Chocolate"
+listaDeCompras[5] = "lasanha"
+
+//console.log(listaDeCompras)
+console.log(listaDeCompras[5])
