fd0cb24a937bb6c0a6e740f40aae909e48aef659
ruby --version <paulinn.rs@gmail.com>
2023-08-29 16:16:47 -0300

    FUNCTION

---
+++ modulo_2/aula4/function.js
@@ -0,0 +1,15 @@
+function IAm() {
+    console.log("Prazer, sou Paulo Ronerto")
+}
+
+function movie(){
+    console.log("Meu filme favorito e Harry Potter")
+}
+
+function club() {
+    console.log("Meu time e o Flamengo")
+}
+
+movie()
+club()
+IAm()
