b8fe80cc5e1f696a5dab3a89ba6c1045e5341554
ruby --version <paulinn.rs@gmail.com>
2023-08-31 16:45:51 -0300

    Funções Recursivas

--- modulo_2/aula9/recursos.js
+++ modulo_2/aula9/recursos.js
@@ -1,3 +1,4 @@
+// Função recursiva é uma função que chama ela mesma repetidamente
 function dividir(num) {
     console.log(num)
     if (num % 2 === 0) {
@@ -7,4 +8,31 @@ function dividir(num) {
     }
 }
 
-dividir(256)
+dividir(16384)
+
+// É fundamental que uma função recursiva tenha uma forma de parar de chamar novas funções
+function dobrar(num) {
+    console.log(num)
+    dobrar(num * 2)
+  }
+ // dobrar(1) 
+ //-> Estoura a pilha de chamadas <-
+
+ // Para construir uma função recursiva, geralmente partimos de um caso de base
+// para garantir que nossa função não vai continuar se chamando para sempre
+// Obs.: !5 (fatorial de 5) = 5 * 4 * 3 * 2 * 1 = 5 * !4
+function fatorial(num) {
+    console.log("num = " + num)
+    if (num === 0) { // caso base
+      return 1
+    } else if (num === 1) { // caso base
+      return 1
+    } else {
+      console.log(num + " * !" + (num - 1))
+      return num * fatorial(num - 1)
+    }
+  }
+  
+  console.log("\n!5 = " + fatorial(5))
+  console.log("\n!0 = " + fatorial(0))
+  console.log("\n!9 = " + fatorial(9))
